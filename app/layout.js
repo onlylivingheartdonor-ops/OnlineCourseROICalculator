eeb0b01b73d1bdd33eadbb912c1fe0740444bea9
hsking1964 <onlylivingbraindonor@gmail.com>
2026-05-08 21:33:07 -0400

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Online Course ROI Calculator</title>
+        <meta
+          name="description"
+          content="Estimate the return on investment (ROI) of an online course based on cost, time, and expected income increase."
+        />
+
+        https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3475627763908800
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
